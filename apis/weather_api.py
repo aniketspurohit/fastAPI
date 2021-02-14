0df69b8a7f7d36a58f16ccc2ffeb74ef3b3a1c30
aniks23 <aniket.purohit35@gmail.com>
2021-02-15 01:37:53 +0530

    Error

--- apis/weather_api.py
+++ apis/weather_api.py
@@ -1,9 +1,36 @@
 from typing import Optional
+import httpx
 import fastapi
+
+from models.location import Location
+from models.umbrella_status import UmbrellaStatus
 
 router = fastapi.APIRouter()
 
 
-@router.get("/api/umbrella")
-def need_an_umbrella(city: str, state: Optional[str] = None):
-    return {"city": city}
+@router.get("/api/umbrella", response_model=Location)
+async def need_an_umbrella(location: Location = fastapi.Depends()):
+    url = f"https://weather.talkpython.fm/api/weather?city={location.city}&country={location.country}&units=imperial"
+
+    if location.state:
+        url += f"&state={location.state}"
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+        data = resp.json()
+
+    weather = data.get("weather", {})
+    category = weather.get("category", "YAYAY")
+    forecast = data.get("forecast", {})
+    temp = forecast.get("temp", 0.0)
+
+    bring = category.lower().strip() == "rain"
+
+    # try:
+    #     return UmbrellaStatus(bring_umbrella=bring, temp=temp)
+    # except ValidationError as e:
+    #     print(e)
+
+    u = UmbrellaStatus(bring_umbrella=bring, temp=temp).json()
+    return u
--- views/home.py
+++ views/home.py
@@ -7,7 +7,7 @@ templates = Jinja2Templates("templates")
 router = fastapi.APIRouter()
 
 
-@router.get("/")
+@router.get("/", include_in_schema=False)  # removes index url from the swagger UI
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
