98ee36e5fb90c8e42dd137622dab60b61af9476d
aniks23 <aniket.purohit35@gmail.com>
2021-02-15 13:32:00 +0530

    [Bugs fixed and basics series complete]

--- apis/weather_api.py
+++ apis/weather_api.py
@@ -1,39 +1,25 @@
-from typing import Optional, List
+from typing import Optional
 import httpx
 import fastapi
 
 from models.location import Location
 from models.umbrella_status import UmbrellaStatus
 
+from weather_service import live_weather_service
+
 router = fastapi.APIRouter()
 
 
 @router.get("/api/umbrella", response_model=UmbrellaStatus)
 async def need_an_umbrella(location: Location = fastapi.Depends()):
-    url = f"https://weather.talkpython.fm/api/weather?city={location.city}&country={location.country}&units=imperial"
-
-    if location.state:
-        url += f"&state={location.state}"
-
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(url)
-        resp.raise_for_status()
-        data = resp.json()
+    data = await live_weather_service.get_live_report(location)
 
     weather = data.get("weather", {})
     category = weather.get("category", "YAYAY")
     forecast = data.get("forecast", {})
     temp = forecast.get("temp", 0.0)
 
-    if category.lower().strip() == "rain":
-        bring = True
-    else:
-        bring = False
+    bring = category.lower().strip() in {"rain", "mist"}
 
-    # try:
-    #     return UmbrellaStatus(bring_umbrella=bring, temp=temp)
-    # except ValidationError as e:
-    #     print(e)
-
-    u = UmbrellaStatus(bring_umbrella=bring, temp=temp)
+    u = UmbrellaStatus(bring_umbrella=bring, temp=temp, weather=category)
     return u
